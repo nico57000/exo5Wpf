c8c051a970443fec5e28ebebd9517dc1a14bcf9d
nico57000 <falcettanicolas@gmail.com>
2019-05-24 09:31:09 +0200

    Ajoutez des fichiers projet.

---
+++ exo5Wpf/Livre.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace exo5Wpf
+{
+    [DataContract]
+    class Livre : INotifyPropertyChanged
+    {
+        private string _auteur;
+        private string _titre;
+        private DateTime _Annee;
+        private string _resum;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [DataMember]
+        public string Auteur
+        {
+            get { return _auteur; }
+            set {
+                if (this._auteur != value)
+                {
+                    this._auteur = value;
+                    this.NotifyPropertyChanged("Auteur");
+                }
+                _auteur = value; }
+        }
+
+        
+        [DataMember]
+        public string Titre
+        {
+            get { return _titre; }
+            set {
+                if (this._titre != value)
+                {
+                    this._titre = value;
+                    this.NotifyPropertyChanged("Titre");
+                }
+                _titre = value; }
+        }
+
+
+        [DataMember]
+        public DateTime annee
+        {
+            get { return _Annee; }
+            set {
+                if (this._Annee != value)
+                {
+                    this._Annee = value;
+                    this.NotifyPropertyChanged("annee");
+                }
+                _Annee = value; }
+        }
+
+
+
+        [DataMember]
+        public string Resum
+        {
+            get { return _resum; }
+            set {
+                if (this._resum != value)
+                {
+                    this._resum = value;
+                    this.NotifyPropertyChanged("Resum");
+                }
+                _resum = value; }
+        }
+
+
+
+        public Livre(string auteur, string titre, DateTime annee)
+        {
+            Auteur = auteur;
+            Titre = titre;
+            this.annee = annee;
+            Resum = "Post haec indumentum regale quaerebatur et ministris fucandae purpurae tortis confessisque pectoralem tuniculam sine manicis textam, Maras nomine quidam inductus est ut appellant Christiani diaconus, cuius prolatae litterae scriptae Graeco sermone ad Tyrii textrini praepositum celerari speciem perurgebant quam autem non indicabant denique etiam idem ad usque discrimen vitae vexatus nihil fateri conpulsus est.";
+        }
+
+        public Livre()
+        {
+            Resum = "Post haec indumentum regale quaerebatur et ministris fucandae purpurae tortis confessisque pectoralem tuniculam sine manicis textam, Maras nomine quidam inductus est ut appellant Christiani diaconus, cuius prolatae litterae scriptae Graeco sermone ad Tyrii textrini praepositum celerari speciem perurgebant quam autem non indicabant denique etiam idem ad usque discrimen vitae vexatus nihil fateri conpulsus est.";
+        }
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+    
+}
---
+++ exo5Wpf/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace exo5Wpf
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private ObservableCollection<Livre> livres = new ObservableCollection<Livre>() {
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            data.ItemsSource = livres;
+            data.IsReadOnly = true;
+        }
+
+        private void Charger(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            open.Filter = "JSON Files | *.json";
+            if (open.ShowDialog() == true)
+            {
+                Stream stm3 = new StreamReader("save.json").BaseStream;
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(ObservableCollection<Livre>));
+                ObservableCollection<Livre> tmp = (ObservableCollection<Livre>)ser.ReadObject(stm3);
+                stm3.Close();
+                livres.Clear();
+                foreach (Livre item in tmp)
+                {
+                    livres.Add(item);
+                }
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+            Stream stm = new StreamWriter("save.json").BaseStream;
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(ObservableCollection<Livre>));
+            ser.WriteObject(stm, livres);
+            stm.Close();
+
+        }
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+            data.IsReadOnly = false;
+        }
+    }
+}
